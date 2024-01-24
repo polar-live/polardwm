e528fe22518e6847dae8f1b33a242d78f4c382b5
polar-live <hhitchcock231@gmail.com>
2024-01-23 22:41:48 -0500

    fixed setgaps error

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -214,7 +214,7 @@ static void setclientstate(Client *c, long state);
 static void setfocus(Client *c);
 static void setfullscreen(Client *c, int fullscreen);
 static void fullscreen(const Arg *arg);
-static void setgaps(const Arg *arg);
+/*static void setgaps(const Arg *arg);*/
 static void setlayout(const Arg *arg);
 static void setmfact(const Arg *arg);
 static void setup(void);
